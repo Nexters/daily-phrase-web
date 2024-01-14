8b8c2a21f00ff4cd6abec976f73583818fbdbbc5
bepyan <bepyan@naver.com>
2024-01-14 22:43:58 +0900

    chore: shadcn-ui combobox

--- packages/back-office/src/components/form-example.tsx
+++ packages/back-office/src/components/form-example.tsx
@@ -5,6 +5,9 @@ import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
+import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
+
+import { cn } from '~/libs/utils';
 
 import { Button } from '~/components/ui/button';
 import {
@@ -28,6 +31,26 @@ import {
   SelectValue,
 } from '~/components/ui/select';
 import { Switch } from '~/components/ui/switch';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from '~/components/ui/command';
+import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
+
+const languages = [
+  { label: 'English', value: 'en' },
+  { label: 'French', value: 'fr' },
+  { label: 'German', value: 'de' },
+  { label: 'Spanish', value: 'es' },
+  { label: 'Portuguese', value: 'pt' },
+  { label: 'Russian', value: 'ru' },
+  { label: 'Japanese', value: 'ja' },
+  { label: 'Korean', value: 'ko' },
+  { label: 'Chinese', value: 'zh' },
+] as const;
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -52,6 +75,9 @@ const formSchema = z.object({
     .email(),
   marketing_emails: z.boolean().default(false).optional(),
   security_emails: z.boolean(),
+  language: z.string({
+    required_error: 'Please select a language.',
+  }),
 });
 
 export function ProfileForm() {
@@ -230,7 +256,64 @@ export function ProfileForm() {
               )}
             />
           </div>
-        </div>
+        </div>{' '}
+        <FormField
+          control={form.control}
+          name='language'
+          render={({ field }) => (
+            <FormItem className='flex flex-col'>
+              <FormLabel>Language</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant='outline'
+                      role='combobox'
+                      className={cn(
+                        'w-[200px] justify-between',
+                        !field.value && 'text-muted-foreground',
+                      )}
+                    >
+                      {field.value
+                        ? languages.find((language) => language.value === field.value)?.label
+                        : 'Select language'}
+                      <ChevronsUpDownIcon className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className='w-[200px] p-0'>
+                  <Command>
+                    <CommandInput placeholder='Search framework...' className='h-9' />
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {languages.map((language) => (
+                        <CommandItem
+                          value={language.label}
+                          key={language.value}
+                          onSelect={() => {
+                            form.setValue('language', language.value);
+                          }}
+                        >
+                          {language.label}
+                          <CheckIcon
+                            className={cn(
+                              'ml-auto h-4 w-4',
+                              language.value === field.value ? 'opacity-100' : 'opacity-0',
+                            )}
+                          />
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                This is the language that will be used in the dashboard.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type='submit'>Submit</Button>
       </form>
     </Form>
