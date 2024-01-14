005eb7fecc380b3b7051a926d21a07f72b2bcdb9
bepyan <bepyan@naver.com>
2024-01-14 22:37:56 +0900

    chore: shadcn-ui switch

--- packages/back-office/src/components/form-example.tsx
+++ packages/back-office/src/components/form-example.tsx
@@ -27,6 +27,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from '~/components/ui/select';
+import { Switch } from '~/components/ui/switch';
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -49,6 +50,8 @@ const formSchema = z.object({
       required_error: 'Please select an email to display.',
     })
     .email(),
+  marketing_emails: z.boolean().default(false).optional(),
+  security_emails: z.boolean(),
 });
 
 export function ProfileForm() {
@@ -58,6 +61,7 @@ export function ProfileForm() {
     defaultValues: {
       username: '',
       bio: '',
+      security_emails: true,
     },
   });
 
@@ -185,6 +189,48 @@ export function ProfileForm() {
             </FormItem>
           )}
         />
+        <div>
+          <h3 className='mb-4 text-lg font-medium'>Email Notifications</h3>
+          <div className='space-y-4'>
+            <FormField
+              control={form.control}
+              name='marketing_emails'
+              render={({ field }) => (
+                <FormItem className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
+                  <div className='space-y-0.5'>
+                    <FormLabel>Marketing emails</FormLabel>
+                    <FormDescription>
+                      Receive emails about new products, features, and more.
+                    </FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch checked={field.value} onCheckedChange={field.onChange} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='security_emails'
+              render={({ field }) => (
+                <FormItem className='flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm'>
+                  <div className='space-y-0.5'>
+                    <FormLabel>Security emails</FormLabel>
+                    <FormDescription>Receive emails about your account security.</FormDescription>
+                  </div>
+                  <FormControl>
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                      disabled
+                      aria-readonly
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
         <Button type='submit'>Submit</Button>
       </form>
     </Form>
