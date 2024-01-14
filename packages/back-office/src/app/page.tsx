8067128288099c6052e7b7e9636fa9de20cbc227
bepyan <bepyan@naver.com>
2024-01-14 22:49:45 +0900

    chore: shadcn-ui date-picker

--- packages/back-office/src/app/page.tsx
+++ packages/back-office/src/app/page.tsx
@@ -2,7 +2,7 @@ import { ProfileForm } from '~/components/form-example';
 
 export default function Page() {
   return (
-    <div className='pt-32 space-y-7'>
+    <div className='py-32 space-y-7'>
       <ProfileForm></ProfileForm>
     </div>
   );
--- packages/back-office/src/components/form-example.tsx
+++ packages/back-office/src/components/form-example.tsx
@@ -4,8 +4,9 @@
 import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { format } from 'date-fns';
 import * as z from 'zod';
-import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
+import { CalendarIcon, CheckIcon, ChevronsUpDownIcon } from 'lucide-react';
 
 import { cn } from '~/libs/utils';
 
@@ -39,6 +40,7 @@ import {
   CommandItem,
 } from '~/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '~/components/ui/popover';
+import { Calendar } from '~/components/ui/calender';
 
 const languages = [
   { label: 'English', value: 'en' },
@@ -77,6 +79,9 @@ const formSchema = z.object({
   security_emails: z.boolean(),
   language: z.string({
     required_error: 'Please select a language.',
+  }),
+  dob: z.date({
+    required_error: 'A date of birth is required.',
   }),
 });
 
@@ -256,7 +261,7 @@ export function ProfileForm() {
               )}
             />
           </div>
-        </div>{' '}
+        </div>
         <FormField
           control={form.control}
           name='language'
@@ -310,6 +315,42 @@ export function ProfileForm() {
               <FormDescription>
                 This is the language that will be used in the dashboard.
               </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='dob'
+          render={({ field }) => (
+            <FormItem className='flex flex-col'>
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={'outline'}
+                      className={cn(
+                        'w-[240px] pl-3 text-left font-normal',
+                        !field.value && 'text-muted-foreground',
+                      )}
+                    >
+                      {field.value ? format(field.value, 'PPP') : <span>Pick a date</span>}
+                      <CalendarIcon className='ml-auto h-4 w-4 opacity-50' />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className='w-auto p-0' align='start'>
+                  <Calendar
+                    mode='single'
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
