cd2b32cee3d7a8005a04cfeed3e50da87fa31dcc
bepyan <bepyan@naver.com>
2024-01-14 22:56:32 +0900

    style: components.json alias

--- packages/back-office/src/components/ui/form.tsx
+++ packages/back-office/src/components/ui/form.tsx
@@ -10,8 +10,8 @@ import {
   useFormContext,
 } from 'react-hook-form';
 
-import { cn } from 'src/libs/utils';
-import { Label } from 'src/components/ui/label';
+import { cn } from '~/libs/utils';
+import { Label } from '~/components/ui/label';
 
 const Form = FormProvider;
 
