96bd96e19c8697b9abe458634a8e5fe9abc29573
bepyan <bepyan@naver.com>
2024-01-22 23:57:31 +0900

    style: cleanup

--- apps/web/src/app/a/[postId]/page.tsx
+++ apps/web/src/app/a/[postId]/page.tsx
@@ -2,7 +2,7 @@ import * as stylex from "@stylexjs/stylex";
 import PostContent from "~/components/post-content";
 import { globalStyles } from "~/styles/globals.style";
 
-export default function PostPage() {
+export default function AppContentPage() {
   return (
     <main {...stylex.props(globalStyles.container)}>
       <PostContent />
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,88 +1,17 @@
-import Image from "next/image";
+import * as stylex from "@stylexjs/stylex";
+import { globalStyles } from "~/styles/globals.style";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main>
-      <div>
-        <p>
-          Get started by editing&nbsp;
-          <code>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div>
-        <Image
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main {...stylex.props(globalStyles.container, styles.container)}>
+      <h1 style={{ marginBottom: 8 }}>매일 글귀</h1>
+      <p>좋은 시, 좋은 명언, 좋은 말 모음</p>
     </main>
   );
 }
+
+const styles = stylex.create({
+  container: {
+    padding: "40px 16px",
+  },
+});
