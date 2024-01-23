a4c473de1fbeb038ca987e2c19de32e6a6cfe4bb
bepyan <bepyan@naver.com>
2024-01-24 01:07:56 +0900

    feat: sidebar ui

--- apps/back-office/src/app/layout.tsx
+++ apps/back-office/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers";
+import AdminLayout from "~/components/admin-layout";
 import { Toaster } from "~/components/ui/sonner";
 import { cn } from "~/libs/utils";
 import "~/styles/globals.css";
@@ -5,14 +7,21 @@ import "~/styles/globals.css";
 export default function RootLayout({
   children,
 }: { children: React.ReactNode }) {
+  const layout = cookies().get("react-resizable-panels:layout");
+  const collapsed = cookies().get("react-resizable-panels:collapsed");
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined;
+
   return (
     <html lang="ko">
-      <body
-        className={cn(
-          "container min-h-screen bg-background font-sans antialiased",
-        )}
-      >
-        {children}
+      <body className={cn(" min-h-screen bg-background font-sans antialiased")}>
+        <AdminLayout
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+        >
+          {children}
+        </AdminLayout>
         <Toaster />
       </body>
     </html>
--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -2,7 +2,7 @@ import { ProfileForm } from "~/components/form-example";
 
 export default function Page() {
   return (
-    <div className="py-32 space-y-7">
+    <div className="p-20 space-y-7">
       <ProfileForm />
     </div>
   );
