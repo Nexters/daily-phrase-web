3478745d9732997f47672054637ee9459ad6ef60
dykim <drdragon777@whatap.io>
2024-01-27 02:03:00 +0900

    feat: back-office 폰트 추가

--- apps/back-office/src/app/layout.tsx
+++ apps/back-office/src/app/layout.tsx
@@ -7,9 +7,17 @@ export default function RootLayout({
 }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        />
+      </head>
       <body
         className={cn(
-          "container min-h-screen bg-background font-sans antialiased",
+          "container min-h-screen bg-background font-pretendard antialiased",
         )}
       >
         {children}
