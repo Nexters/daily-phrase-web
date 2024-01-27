c82621ff5b807bf4385f983976b015518760417d
dykim <drdragon777@whatap.io>
2024-01-27 16:45:27 +0900

    feat: 페이지네이션 로직 훅으로 분리

--- apps/back-office/src/app/page.tsx
+++ apps/back-office/src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import ActionBarLayout from "~/components/manage-service/action-bar-layout";
 import DisplayDataNumSelect from "~/components/manage-service/display-data-num-select";
 import { ManagePagination } from "~/components/manage-service/manage-pagination";
@@ -9,64 +8,26 @@ import { Button } from "~/components/ui/button";
 import { Checkbox } from "~/components/ui/checkbox";
 import DataTable from "~/components/ui/data-table";
 
-import {
-  OnCheck,
-  OnDelete,
-} from "~/components/manage-service/manage-service.type";
-import { PaginationData } from "~/types/pagination";
+import { useManagePagination } from "~/components/manage-service/hooks";
 import { getPhraseItemListMocks, rowsPerPageOptions } from "./meta";
 
-/** @description 서버 데이터 개수(변경 필요) */
-const totalRows = 100; // 전체 데이터 행의 수
+/** @todo 서버 데이터 개수(변경 필요) */
+const totalRows = 100; // 전체 데이터 개수
 
 export default function Page() {
-  /** @todo 페이지네이션 상태 관리 필요, 현재는 프론트에서 페이지네이션 관리 */
-  const [pagination, setPagination] = useState<PaginationData>({
-    current: 1,
-    limit: Number(rowsPerPageOptions[0].value),
-    size: Math.ceil(totalRows / Number(rowsPerPageOptions[0].value)),
-  });
-
-  const startIndex = (pagination.current - 1) * pagination.limit;
-  const endIndex = startIndex + pagination.limit;
-
-  /** @todo 서버 데이터로 교체 */
-  const data = getPhraseItemListMocks(totalRows).slice(startIndex, endIndex);
-
-  const [checkedItems, setCheckedItems] = useState<Array<number>>([]);
-
-  const onRowSizeChange = (rowsPerPage: string) => {
-    /** @todo 숫자형변환했을 때 숫자가 아니면 오류 뜨도록 */
-    setPagination({
-      current: 1,
-      limit: Number(rowsPerPage),
-      size: Math.ceil(totalRows / Number(rowsPerPage)), // 총 페이지 수 재계산
-    });
-    setCheckedItems([]);
-  };
-
-  const onPageMove: Parameters<typeof ManagePagination>[0]["onPageMove"] = (
+  const {
     pagination,
-    next,
-  ) => {
-    setCheckedItems([]);
-    setPagination((prev) => ({ ...prev, current: next }));
-  };
-
-  const currentPageDataIds = data.map((item) => item.id);
-
-  const isAllDeleteChecked = checkedItems.length === currentPageDataIds.length;
-
-  const onCheckAllDelete: OnCheck = (checked) => {
-    setCheckedItems(checked ? currentPageDataIds : []);
-  };
-
-  const isDeleteChecked = (id: number) => checkedItems.includes(id);
-
-  const onDeleteCheck: OnDelete = (id, checked) => {
-    if (checked) setCheckedItems((prev) => [...new Set(prev).add(id)]);
-    else setCheckedItems((prev) => prev.filter((itemId) => itemId !== id));
-  };
+    transformedData,
+    onRowSizeChange,
+    onPageMove,
+    isAllDeleteChecked,
+    onCheckAllDelete,
+    isDeleteChecked,
+    onDeleteCheck,
+  } = useManagePagination(
+    getPhraseItemListMocks(totalRows),
+    Number(rowsPerPageOptions[0].value),
+  );
 
   return (
     <div className="py-32 space-y-7">
@@ -97,7 +58,7 @@ export default function Page() {
       />
       <DataTable
         columns={getManageTableColumns(isDeleteChecked, onDeleteCheck)}
-        data={data}
+        data={transformedData}
         NoDataMsg={
           <span className="w-full inline-block text-center text-slate-600 text-base">
             현재 작성 된 글이 없습니다.
---
+++ apps/back-office/src/components/manage-service/hooks/index.ts
@@ -0,0 +1,3 @@
+import useManagePagination from "./useManagePagination";
+
+export { useManagePagination };
