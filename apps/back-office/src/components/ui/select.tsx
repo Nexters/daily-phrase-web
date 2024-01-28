c84b785434e2ad35e0451a180e7a557bbfc03a0b
dykim <drdragon777@whatap.io>
2024-01-28 23:04:44 +0900

    fix: Select icon 조건부 렌더링 수정

--- apps/back-office/src/components/ui/select.tsx
+++ apps/back-office/src/components/ui/select.tsx
@@ -135,7 +135,7 @@ const SelectItem = React.forwardRef<
     )}
     {...props}
   >
-    {!hasCheckIcon && (
+    {hasCheckIcon && (
       <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
         <SelectPrimitive.ItemIndicator>
           <Check className="h-4 w-4" />
