517538f1151a3f5a04f71529add71f5b3fb958cf
dykim <drdragon777@whatap.io>
2024-01-29 02:18:14 +0900

    chore: layout css부분 layout에 옮기기

--- apps/back-office/src/app/login/page.tsx
+++ apps/back-office/src/app/login/page.tsx
@@ -2,10 +2,10 @@ import LoginForm from "~/components/login/login-form";
 
 export default function Page() {
   return (
-    <div className="absolute top-1/2 -translate-y-2/4 left-1/2 -translate-x-2/4 w-[425px] m-auto p-6 rounded-lg border-[1px] border-slate-300">
+    <>
       {/** @todo typography 컴포넌트 필요 */}
       <span className="text-lg font-semibold leading-7">Login</span>
       <LoginForm />
-    </div>
+    </>
   );
 }
