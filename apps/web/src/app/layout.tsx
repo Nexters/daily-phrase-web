0f08dbecf2d2c528891100a25e7e985376c23ede
dykim <drdragon777@whatap.io>
2024-01-30 02:10:49 +0900

    feat: web react-query 세팅

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import * as stylex from "@stylexjs/stylex";
 import type { Metadata } from "next";
+import QueryProvider from "~/providers/QueryProvider";
 import "~/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body {...stylex.props()}>{children}</body>
+      <body {...stylex.props()}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
---
+++ apps/web/src/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { queryClient } from "~/apis/react-query/queryClient";
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
