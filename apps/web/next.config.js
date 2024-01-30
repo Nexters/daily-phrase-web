a694a64629da76e2df94c94425a299f289396951
dykim <drdragon777@whatap.io>
2024-01-31 00:13:34 +0900

    chore: env next config 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,4 +3,9 @@ const stylexPlugin = require("@stylexjs/nextjs-plugin");
 
 module.exports = stylexPlugin({
   rootDir: __dirname,
-})({});
+})({
+  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+  },
+});
