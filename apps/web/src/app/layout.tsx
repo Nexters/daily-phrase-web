c988540d6109fc26a6ac94665083530c2a2d458a
bepyan <bepyan@naver.com>
2024-02-02 00:32:39 +0900

    chore: apply head link to layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -15,6 +15,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css"
+        />
+      </head>
       <body {...stylex.props()}>
         <QueryProvider>{children}</QueryProvider>
       </body>
