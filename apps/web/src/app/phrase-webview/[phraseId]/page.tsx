cc807734d7e10c91a9fc1a357f0722894c4ba540
bepyan <bepyan@naver.com>
2024-02-03 14:22:26 +0900

    feat: webview 데이터 연동

--- apps/web/src/app/phrase-webview/[phraseId]/page.tsx
+++ apps/web/src/app/phrase-webview/[phraseId]/page.tsx
@@ -1,11 +1,20 @@
 import * as stylex from "@stylexjs/stylex";
+import { apis } from "~/apis";
+import { Phrase } from "~/apis/phrase-api/type";
 import PhraseContent from "~/components/phrase-content";
 import { globalStyles } from "~/styles/globals.stylex";
 
-export default function PhraseWebviewPage() {
+export default async function PhraseWebviewPage({
+  params,
+}: {
+  params: { phraseId: string };
+}) {
+  const res = await apis.adminApi.getPhrase(params.phraseId);
+  const phrase: Phrase = res.result;
+
   return (
     <main {...stylex.props(globalStyles.container)}>
-      <PhraseContent />
+      <PhraseContent phrase={phrase} />
     </main>
   );
 }
