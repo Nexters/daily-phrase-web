bca29b745c29ca31744c4c522889498c7284130f
dykim <drdragon777@whatap.io>
2024-02-10 11:19:39 +0900

    chore: redirect 추가

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/phrase-web/1",
+        permanent: true,
+      },
+    ];
+  },
 };
