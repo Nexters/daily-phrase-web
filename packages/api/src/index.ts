ccfff53d0e0ccabeb0af132fb6f3d9bef0211aad
bepyan <bepyan@naver.com>
2024-02-13 02:43:20 +0900

    feat: type

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -2,7 +2,7 @@ export * from "./fetch";
 export * from "./phrase.type";
 
 export interface ApiData<T> {
-  isSuccess: "false" | "true";
+  isSuccess: boolean;
   code: string;
   message: string;
   result: T;
