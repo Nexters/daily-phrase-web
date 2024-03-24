421aa708ef467fc4017a0bc99d16d5b2f426132f
dykim <drdragon777@whatap.io>
2024-03-24 22:16:32 +0900

    feat: web seo 개선

--- apps/web/src/app/phrase-web/[phraseId]/page.tsx
+++ apps/web/src/app/phrase-web/[phraseId]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata, ResolvingMetadata } from "next";
 import { apis } from "~/apis";
 import AppDownloadModal from "~/components/app-download-modal";
 import AppFooter from "~/components/app-footer";
@@ -8,6 +9,10 @@ import PhraseList from "./phrase-list";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
+interface Props {
+  params: { phraseId: string };
+}
 
 export default async function PhraseWebPage({
   params,
@@ -28,4 +33,32 @@ export default async function PhraseWebPage({
       <AppDownloadModal />
     </main>
   );
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.phraseId;
+  const res = await apis.phraseApi.getPhrase(id);
+  const phrase = res.result;
+
+  const url = `https://www.daily-phrase.com/phrase-web/${id}`;
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: phrase.title,
+    description: phrase.content,
+    openGraph: {
+      title: phrase.title,
+      images: [...previousImages, phrase.imageUrl],
+      description: phrase.content,
+      url,
+    },
+    twitter: {
+      title: phrase.title,
+      images: [...previousImages, phrase.imageUrl],
+      description: phrase.content,
+    },
+  };
 }
--- apps/web/src/app/phrase-webview/[phraseId]/page.tsx
+++ apps/web/src/app/phrase-webview/[phraseId]/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata, ResolvingMetadata } from "next";
 import { apis } from "~/apis";
 import PhraseContent from "~/components/phrase-content";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
+interface Props {
+  params: { phraseId: string };
+}
 
 export default async function PhraseWebviewPage({
   params,
@@ -17,4 +22,32 @@ export default async function PhraseWebviewPage({
       <PhraseContent phrase={phrase} />
     </main>
   );
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const id = params.phraseId;
+  const res = await apis.phraseApi.getPhrase(id);
+  const phrase = res.result;
+
+  const url = `https://www.daily-phrase.com/phrase-webview/${id}`;
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: phrase.title,
+    description: phrase.content,
+    openGraph: {
+      title: phrase.title,
+      images: [...previousImages, phrase.imageUrl],
+      description: phrase.content,
+      url,
+    },
+    twitter: {
+      title: phrase.title,
+      images: [...previousImages, phrase.imageUrl],
+      description: phrase.content,
+    },
+  };
 }
